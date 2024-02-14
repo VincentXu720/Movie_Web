c6884a0f21367adfcb797f3425e1043bcc54265b
alex00720 <eafeaef2@gmail.com>
2024-02-14 18:16:34 +0800

    2/14 Fetch Change to ajax

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # Movie_Web
+=======
+# Movie_Web# Movie_Web
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
 
 ## Website
 ![image](https://github.com/VincentXu720/Movie_Web/blob/main/projectImg/Movie.png)
@@ -11,7 +15,11 @@
 -Sass and Bootstrap 5 改變網頁的樣式<br>
 
 ## JQuery/JS/Swiper
+<<<<<<< HEAD
 -Fetch去讀取TMDB的API資料<br>
+=======
+-使用ajax串接TMDB的API資料<br>
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
 -並用 JQuery 進行網頁資料的呈現<br>
 -Swiper 主頁的電影輪播
 
@@ -23,6 +31,10 @@
 https://vincentxu720.github.io/Movie_Web/
 
 ## Github
+<<<<<<< HEAD
 https://github.com/VincentXu720
 
 
+=======
+https://github.com/VincentXu720
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
--- js/movie.js
+++ js/movie.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const key = "api_key=80225fbb01c8c984e45fbc97dbc1e208";
+  const key = "api_key=80225fbb01c8c984e45fbc97dbc1e208"
   const Movie_url = "https://api.themoviedb.org/3";
   const Poster_url = "https://image.tmdb.org/t/p/original";
   
@@ -8,13 +8,21 @@ $(document).ready(function () {
   
   $(".Home").click((e) => {
     e.stopPropagation();
+<<<<<<< HEAD
     const SwiperBox = ` 
+=======
+    const SwiperContainer = ` 
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
         <div class="swiper mySwiper">
           <div class="swiper-wrapper bg-transparent w-100 d-flex">
           </div>
         </div>
       `;
+<<<<<<< HEAD
     $(".movie_Box").prepend(SwiperBox); // 將元素插在所選的元素前面
+=======
+    $(".movie_Box").prepend(SwiperContainer); // 將元素插在所選的元素前面
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
     if($('.ChangePage').length === 0){
       const changeButton = `
         <div class="d-flex justify-content-center w-100 ChangePage">
@@ -89,6 +97,7 @@ $(document).ready(function () {
       });
   }
 
+<<<<<<< HEAD
   async function FetchMovie(url,count){
     await fetch(Movie_url + url + key + "&page=" + count)
     .then((res) => res.json())
@@ -122,6 +131,64 @@ $(document).ready(function () {
     });
   }
 
+=======
+  // 處理導覽列電影 Container 內容
+  async function FetchMovie(url,count){
+    let MovieContent = "";
+    // 讀取 Movie 資料
+    const MovieContainer = await $.ajax({
+      url: Movie_url + url + key + "&page=" + count,
+      method:"GET",
+      dataType:"JSON",
+      success:function(res){
+        return res
+      },
+      error:function(error){
+        console.log(error)
+      }
+    })
+    // 把資料按照Json中的順序放到TranslateMovie()裡
+    // 防止電影的位置改變
+    for( const movie of MovieContainer.results){
+      await TranslateMovie(movie,count)
+    }
+    $(".movie_container").html(MovieContent);
+    $(".page").html(count)
+    
+    // 讀取 Movie Translate 的資料
+    async function TranslateMovie(movie,count){
+      const MovieTranslate = await $.ajax({ 
+        url: Movie_url+`/movie/${movie.id}/translations?`+key,
+        method:"GET",
+        dataType:"JSON",
+        success:function(res){
+          return res
+        },
+        error:function(error){
+          console.log(error)
+        }
+      })
+      // 配對兩筆資料並把資料放到MovieContent變數裡
+      $.each(MovieTranslate.translations,(index,ID)=>{
+        if(ID.iso_639_1 === "zh" && ID.iso_3166_1 === "TW"){
+          if(movie.poster_path && ID.data.title){
+            MovieContent += `
+              <div class="d-flex flex-column mb-3 movie_content">
+                <div class="movie_poster">
+                  <img class="" src="${Poster_url + movie.poster_path}">
+                </div>
+                <div class="d-flex justify-content-center movie_info">
+                  <h4 class="fs-6 title">${ID.data.title}</h4>
+                </div>
+              </div>      
+            `;
+          }
+        }
+      })
+    }
+  }
+  
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
   function clickPage(){
     const changeButton = `
       <div class="d-flex justify-content-center w-100 ChangePage">
@@ -134,21 +201,36 @@ $(document).ready(function () {
     $(".page").html(Home_count)
   }
   async function HomePage(Home_count) {
+<<<<<<< HEAD
+=======
+    let MovieContent = "";
+    SwiperBox();
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
     // 當button重新放入時，由於Home_count是在外部被宣告的，所以導致這裡進來的Home_count會是undefined
     if(Home_count === undefined){
       Home_count = 1;
     }
     const Home_url ="/discover/movie?include_adult=false&include_video=false&language=en-US.desc&page=" + Home_count + "&";
+<<<<<<< HEAD
     SwiperBox();
     await fetch(Movie_url + Home_url + key)
       .then((res) => res.json())
       .then((Data) => {
         // 判定是否還能抓到資料，如果不行就是undefined
         if (Data.results === undefined) {
+=======
+    const getMovie = await $.ajax({
+      url:Movie_url + Home_url + key,
+      method:"GET",
+      dataType:"JSON",
+      success:function(res){
+        if(res.results === undefined){
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
           count = 1;
           HomePage(Home_count);
-          return;
+          return res;
         }
+<<<<<<< HEAD
         let MovieContent = "";
         $.each(Data.results, (index, movie) => {
           if(movie){
@@ -202,6 +284,64 @@ $(document).ready(function () {
       $(".down").off("click").click((e)=>{
         e.stopPropagation()
         $("html,body").animate({
+=======
+
+      }
+    })
+    for(const movie of getMovie.results){
+      await TranslateMovie(movie)
+    }
+    $(".movie_container").html(MovieContent);
+    async function TranslateMovie(movie){
+      const translate = await $.ajax({
+        url:Movie_url+`/movie/${movie.id}/translations?`+key,
+        method:"GET",
+        dataType:"JSON",
+        success:function(res){
+          return res
+        }
+      })
+      $.each(translate.translations,(index,ID)=>{
+        if(ID.iso_639_1 === "zh" && ID.iso_3166_1 === "TW"){
+          if(movie.poster_path && ID.data.title){
+            MovieContent += `
+              <div class="d-flex flex-column mb-3 movie_content">
+                <div class="movie_poster">
+                  <img class="" src="${Poster_url + movie.poster_path}">
+                </div>
+                <div class="d-flex justify-content-center movie_info">
+                  <h4 class="fs-6 title">${ID.data.title}</h4>
+                </div>
+              </div>      
+            `;
+          }
+        }
+      })
+    }
+    $('.page').html(Home_count)
+    HomePageControl(Home_count);
+    function HomePageControl(Home_count) {
+      if(Home_count === 1){
+        $(".up").click(async (e)=>{
+          e.stopPropagation()
+          $("html,body").animate({
+            scrollTop:0
+          })
+        })
+      }else{
+        $(".up").click(async(e)=>{
+          e.stopPropagation()
+          await $("html,body").animate({
+            scrollTop:0
+          })
+          Home_count --
+          HomePage(Home_count)
+        })
+      }
+      $(".down").off("click").click(async(e)=>{
+        e.stopPropagation()
+        await $("html,body").animate({
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
           scrollTop:0
         })
         Home_count ++
@@ -212,6 +352,7 @@ $(document).ready(function () {
 
   // 製造Swiper中的內容
   async function SwiperBox() {
+<<<<<<< HEAD
     const TopPopular_url = "/movie/popular?language=en-US&";
     await fetch(Movie_url + TopPopular_url + key)
       .then((res) => res.json())
@@ -250,19 +391,78 @@ $(document).ready(function () {
   }
 
   function initSwiper(){
+=======
+    let SwiperContainer = "";
+    let TopFive = [];
+    const TopPopular_url = "/movie/popular?language=en-US&";
+    const Movie = await $.ajax({
+      url:Movie_url + TopPopular_url + key,
+      method:"GET",
+      dataType:"JSON",
+      success:function(res){
+        return res
+      },
+      error:function(err){
+        console.log(err)
+      }
+    })
+    for(let i=0;i<5;i++){
+      TopFive.push(Movie.results[i])
+    }
+    for(let movie of TopFive){
+      await SwiperView(movie)
+    }
+    // 再進行初始化
+    initSwiper()
+
+    async function SwiperView(movie){
+      const MovieTranslate = await $.ajax({
+        url:Movie_url+`/movie/${movie.id}/translations?`+key,
+        method:"GET",
+        dataType:"JSON",
+        success:function(res){
+          return res
+        },
+        error:function(err){
+          console.log(err)
+        }
+      })
+      $.each(MovieTranslate.translations,(index,ID)=>{
+        if(ID.iso_639_1 === "zh" && ID.iso_3166_1 === "TW"){
+          SwiperContainer += `
+            <div class="swiper-slide" style=background-image:url(${Poster_url + movie.backdrop_path})>
+                <div class="black">
+                    <h2>${ID.data.title}</h2>
+                    <h4>${movie.original_title}</h4>
+                    <span>${movie.release_date}</span>
+                    <p>${ID.data.overview}</p>
+                </div>
+            </div> 
+          `;
+        }
+        $(".swiper-wrapper").html(SwiperContainer);
+      })
+    }
+  }
+  async function initSwiper(){
+    console.log('yes')
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
     // Swiper JS
     // 要放在抓取電影內容的API後面才能進行初始化
     const mySwiper = new Swiper(".mySwiper", {
       dynamicBullets: true,
-      loop: true,
+      loop:true,
       autoplay: {
-        delay: 1000,
+        delay: 500,
       },
       pagination: {
         el: ".swiper-pagination",
       },
     });
   }
+<<<<<<< HEAD
 
+=======
+>>>>>>> eda98f9 (2/14 fetch changed to ajax)
 });
 
